58ad31d1bc3c88383a8b3d1a9be01cadfed4c87e
ns_abir <abircoxsbazar@gmail.com>
2019-08-04 13:35:08 +0600

    Youtube Player API

---
+++ app/src/main/java/com/coxtunes/youtubeapi/MainActivity.java
@@ -0,0 +1,49 @@
+package com.coxtunes.youtubeapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    private YouTubePlayerView youTubePlayerView;
+    private Button play;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play = findViewById(R.id.button);
+        youTubePlayerView = findViewById(R.id.youtube_player);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+                youTubePlayer.loadVideo("3jZ5vnv-LZc");
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                youTubePlayerView.initialize(PlayerConfig.API_KEY,onInitializedListener);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/coxtunes/youtubeapi/PlayerConfig.java
@@ -0,0 +1,10 @@
+package com.coxtunes.youtubeapi;
+
+public class PlayerConfig {
+
+    public PlayerConfig() {
+    }
+
+    public static final String API_KEY = "AIzaSyA_2wFc5Uhd7C3p5QyB5UtqW4i7V1c_xC0";
+
+}
